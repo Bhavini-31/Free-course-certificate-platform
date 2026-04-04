dfdc20138e60ae7fee5df26871176570ca3abf2b
Bhavini Baghel <bhavinibaghel2005@gmail.com>
2026-04-04 21:14:47 +0530

    I added admin panel and a simple view page

---
+++ free_certificate_course_project/course_platform/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoursePlatfromConfig(AppConfig):
+    name = 'course_platform'
---
+++ free_certificate_course_project/course_platform/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    platform = models.CharField(max_length=100)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.title
